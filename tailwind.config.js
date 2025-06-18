eb720aa9ea20f8f14538206640845f53b7ac9424
SimonNRisk <76711152+SimonNRisk@users.noreply.github.com>
2025-06-18 00:12:38 -0400

    use inter as global font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Inter", "sans-serif"],
+			},
+		},
+	},
+
 	content: [
 		"./pages/**/*.{js,ts,jsx,tsx}",
 		"./components/**/*.{js,ts,jsx,tsx}",
