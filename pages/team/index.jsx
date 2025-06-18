927609ad2df7560f8cdcb2681096985a92ba5746
SimonNRisk <76711152+SimonNRisk@users.noreply.github.com>
2025-06-18 19:30:50 -0400

    integrate components into index jsx for team

--- pages/team/index.jsx
+++ pages/team/index.jsx
@@ -1,54 +1,12 @@
 import Layout from "../../components/layout";
 import Image from "next/image";
 import Nav from "../Nav";
-import { ClubMembers, Positions } from "../../data/TeamData.js";
-import { AiFillLinkedin } from "react-icons/ai";
 import Head from "next/head";
 import Footer from "../../components/Footer";
+import { ClubMembers, Positions } from "../../data/TeamData.js";
+import { TeamRoleSection } from "./components/TeamRoleSection";
 
 const Team = () => {
-	const renderClubMembers = () => {
-		const renderSpecialRole = (student) => {
-			if (student.subPosition) {
-				return <p className="special-role">{student.subPosition}</p>;
-			}
-		};
-
-		return Positions.map((category) => (
-			<>
-				<p className="position-name">
-					{category + (category.endsWith("s") ? "" : "s")}
-				</p>
-				<div className="student-photos">
-					{ClubMembers.filter(
-						(student) => student.position === category
-					).map((student, i) => (
-						<div key={i} className="student-container">
-							<Image
-								alt="Student photo"
-								className="student-image"
-								src={student.image}
-								width={500}
-								height={500}
-							/>
-							<p className="student-name">{student.name}</p>
-							{renderSpecialRole(student)}
-
-							<a
-								className="linkedIn"
-								rel="noreferrer"
-								target="_blank"
-								href={student.linkedIn}
-							>
-								<AiFillLinkedin />
-							</a>
-						</div>
-					))}
-				</div>
-			</>
-		));
-	};
-
 	return (
 		<>
 			<Head>
@@ -56,13 +14,30 @@ const Team = () => {
 				<title>QTMA</title>
 				<meta name="og:title" content={"QTMA"} />
 			</Head>
+
 			<Nav />
+
 			<Layout background={"#edf5fc"}>
 				<div className="history-container container">
 					<h1 className="section-title">2023/2024 Team</h1>
-					{renderClubMembers()}
+
+					{Positions.map((role) => {
+						const members = ClubMembers.filter(
+							(m) => m.position === role
+						);
+						if (members.length === 0) return null;
+
+						return (
+							<TeamRoleSection
+								key={role}
+								role={role}
+								members={members}
+							/>
+						);
+					})}
 				</div>
 			</Layout>
+
 			<Footer />
 		</>
 	);
