6cdce119aff3b16f7c13e216de23131c255ee647
SimonNRisk <76711152+SimonNRisk@users.noreply.github.com>
2025-06-18 19:36:04 -0400

    Add qtma custom dark colour into config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+
 	theme: {
 		extend: {
+			colors: {
+				qtmaPrimaryDark: "#2C3357",
+			},
 			fontFamily: {
 				sans: ["Inter", "sans-serif"],
 			},
 		},
 	},
-
-	content: [
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
 };
