522fc035ebaa61093e495abf9a79eb9f46ea3c10
SimonNRisk <76711152+SimonNRisk@users.noreply.github.com>
2025-06-18 21:55:43 -0400

    ie

--- pages/team/index.jsx
+++ pages/team/index.jsx
@@ -5,7 +5,7 @@ import Head from "next/head";
 import Footer from "../../components/Footer";
 import { ClubMembers, Positions } from "../../data/TeamData.js";
 import { TeamRoleSection } from "../components/TeamRoleSection";
-import { LinkBox } from "../../components/TeamRoleSection.tsx";
+import { LinkBox } from "../../components/LinkBox";
 
 const Team = () => {
 	return (
